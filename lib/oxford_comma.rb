0c49826136d68108774fad52a3b54577f591918d
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 17:48:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,14 @@
 def oxford_comma(array)
   if array.length == 1
     result = array[0]
+<<<<<<< HEAD
   elsif array.length == 2 
     result = "#{array[0]} and #{array[1]}"
   else
     result = "#{array[0, array.length-1].join(', ')}, and #{array.last}"
+=======
+  else
+    result = "#{array[0, array.length-1].join(', ')} and #{array.last}"
+>>>>>>> 70f700de1de71efd163f88c254f23287194db169
   end
 end
