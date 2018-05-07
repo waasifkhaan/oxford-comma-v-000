aca61b07b14002799d53ed3540b8204ab0f18c8c
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 04:48:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    result = array[0]
+  elsif array.length == 2 
+    result = "#{array[0]} and #{array[1]}"
+  else
+    result = "#{array[0, array.length-1].join(', ')}, and #{array.last}"
+  end
 end
